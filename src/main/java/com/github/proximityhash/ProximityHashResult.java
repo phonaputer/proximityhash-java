1ae31a567f3267d88598efa9ea97f25c96679fa2
phonaputer <njerome@uwalumni.com>
2019-05-31 19:45:46 +0900

    Removed Lombok dependency. Added getters to ProximityHashResult with Javadocs.

--- src/main/java/com/github/proximityhash/ProximityHashResult.java
+++ src/main/java/com/github/proximityhash/ProximityHashResult.java
@@ -1,28 +1,36 @@
 package com.github.proximityhash;
 
-import lombok.Getter;
-
 import java.util.Set;
 
 /**
  * Results from {@link com.github.proximityhash.ProximityHash#findGeohashesWithinRadius(GeoPoint, double, int)}.
  */
-@Getter
 public class ProximityHashResult {
 
-    /**
-     * All geohashes of the given precision which lie entirely within the radius.
-     */
     private Set<String> hashesWithinRadius;
-
-    /**
-     * All geohashes of the given precision which lie partially but not entirely within the radius.
-     */
     private Set<String> hashesPartiallyWithinRadius;
 
     public ProximityHashResult(Set<String> hashesWithinRadius, Set<String> hashesPartiallyWithinRadius) {
         this.hashesWithinRadius = hashesWithinRadius;
         this.hashesPartiallyWithinRadius = hashesPartiallyWithinRadius;
+    }
+
+    /**
+     * Get all geohashes of the given precision which lie entirely within the radius.
+     *
+     * @return A set of geohash strings.
+     */
+    public Set<String> getHashesWithinRadius() {
+        return hashesWithinRadius;
+    }
+
+    /**
+     * Get all geohashes of the given precision which lie partially but not entirely within the radius.
+     *
+     * @return A set of geohash strings.
+     */
+    public Set<String> getHashesPartiallyWithinRadius() {
+        return hashesPartiallyWithinRadius;
     }
 
 }
